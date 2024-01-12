4bb1c747da3644dfba670707d8e44696695b5956
evbauer <18405113+evbauer@users.noreply.github.com>
2024-01-12 12:51:43 -0500

    [ci skip] some extra plotting stuff

--- kap/plotter/plotter.py
+++ kap/plotter/plotter.py
@@ -24,7 +24,7 @@ with open('kap_plotter.dat') as f:
 kapDT, Yran, Xran = parse('kap_plotter.dat')
 
 # set up plot and labels
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(4,3))
 ax.set_title(title)
 ax.set_xlabel(xlabel)
 ax.set_ylabel(ylabel)
@@ -32,21 +32,33 @@ ax.set_xlim(Xran.min(), Xran.max())
 ax.set_ylim(Yran.min(), Yran.max())
 
 # set up color map
-cmap = copy.copy(mpl.cm.get_cmap("viridis"))
+cmap = copy.copy(mpl.cm.get_cmap("coolwarm"))
 cmap.set_over('white')
 cmap.set_under('black')
 
 # set color bar limits
 # None will auto-set limits
-cbar_min = None
-cbar_max = None
+cbar_min = -16
+cbar_max = 2
 
 pcol = ax.pcolormesh(Xran, Yran, kapDT[...,2], shading='nearest', cmap=cmap, vmin=cbar_min, vmax=cbar_max)
 pcol.set_edgecolor('face')
 cax = fig.colorbar(pcol, extend='both')
 cax.set_label('')
 
+# if x-y is logRho-logT, plot edges of logR
+if(xlabel=='log10(Rho)' and ylabel=='log10(T)'):
+    logRho = Xran
+    logR_max = 1
+    logR_min = -8
+    logT_max = (logRho - logR_min + 18)/3
+    logT_min = (logRho - logR_max + 18)/3
+    ax.plot(logRho,logT_max,c='k',ls='-.',alpha=0.5)
+    ax.plot(logRho,logT_min,c='k',ls='-.',alpha=0.5)
+    #ax.text(-5,7.1,r'logR=-8',rotation=53,fontsize='small')
+    
+
 # save figure
-fig.savefig('kap_plotter.png', dpi=150)
+fig.savefig('kap_plotter.png', dpi=300)
 
 plt.show()
